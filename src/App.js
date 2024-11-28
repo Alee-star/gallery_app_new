73e4025c4733ab97518c8897a69dc881c7e511ff
Aleena <toaleenavshaji@gmail.com>
2024-11-28 16:46:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Heading from "./components/Header/Heading";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
 }
 
 export default App;
