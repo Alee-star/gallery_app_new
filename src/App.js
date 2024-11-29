19b13d72dc729db3d16aff9a0ff25af248cf42d4
Aleena <toaleenavshaji@gmail.com>
2024-11-29 11:28:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./App.css";
 import Heading from "./components/Header/Heading";
 import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
 
 function App() {
   return (
